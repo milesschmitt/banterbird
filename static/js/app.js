46b63de28e99239ca2303ed184c70887d8577b81
milesschmitt <miles.schmitt@ascte.org>
2025-04-22 13:35:42 -0500

    miles

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,9 @@
 const username = "admin";
 
 function renderPost(post) {
-    const template = document.getElementById("post-template").content.cloneNode(true);
+    const template = document
+        .getElementById("post-template")
+        .content.cloneNode(True);
     template.querySelector(".username").innerText = post.username;
     template.querySelector(".message").innerText = post.message;
     document.getElementById("feed").appendChild(template);
@@ -9,8 +11,20 @@ function renderPost(post) {
 
 function submitPost() {
     const message = document.getElementById("postInput").value;
-    console.log("Would post:", message);
-    alert("Tweet submitted (not really yet)");
+    try{
+        const response = fetch("/api/add_post", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username, message
+            })
+        });
+    }catch (error)
+    {
+        console.log("Post Failed :cry:", error)
+    }
 }
 
 window.onload = async() => {

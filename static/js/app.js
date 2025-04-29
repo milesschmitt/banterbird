b59800a9f5d6c9f77e6b77d986cd3535f0d6339e
milesschmitt <miles.schmitt@ascte.org>
2025-04-29 13:45:39 -0500

    Peterbird updated with usernames working

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,7 @@
-const username = "admin";
+let username = localStorage.getItem("username");
+if (!username) {
+    window.location.href = "/login";
+}
 
 function renderPost(post, isNew = false) {
     const template = document
@@ -32,6 +35,7 @@ async function submitPost() {
 
 window.onload = async () => {
     try {
+        document.getElementById("username").innerText = username;
       const response = await fetch("/api/posts");
       const posts = await response.json();
       posts.forEach((post) => renderPost(post));
